64f28e90ac1d41b3667b785a576aa7baaf1aab47
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2023-09-06 08:54:04 -0700

    compile js, add new svg icons

---
+++ resources/views/components/svg/checkmark-icon.blade.php
@@ -0,0 +1,3 @@
+<svg width="26" height="20" viewBox="0 0 26 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path id="Vector 1673" d="M1 10.7143L8.28571 18L25.2857 1" stroke="black" stroke-width="2"/>
+</svg>
