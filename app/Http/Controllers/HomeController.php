46bdf20169392bd2688c99d3bbd4892d318dfd67
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2023-09-06 08:54:50 -0700

    Remove projects from home controller for now

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,7 +30,7 @@ class HomeController extends Controller
     public function index()
     {
         return view('home-new', [
-            'projects' => $this->getTopTen(),
+//            'projects' => $this->getTopTen(),
         ]);
     }
 }
