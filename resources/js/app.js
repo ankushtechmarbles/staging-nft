785b72dc3352813e47d6840fc3e1ddade50dd7c7
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2023-09-06 08:56:12 -0700

    Add thirdweb to app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,12 @@
 require('./bootstrap');
-window.thirdweb = require('@thirdweb-dev/sdk')
+const thirdweb = require('@thirdweb-dev/sdk');
+const { MetaMaskWallet, PaperWallet } = require("@thirdweb-dev/wallets");
+
+window.thirdweb = thirdweb;
+window.MetaMask = new MetaMaskWallet({
+    clientId: '44aa3ec3d8ffe49358a72c91c8e99e83'
+});
+window.PaperWallet = new PaperWallet({
+    chain: 'mumbai',
+    paperClientId: '69029336-0647-469b-8348-7a947f8c1609'
+});
