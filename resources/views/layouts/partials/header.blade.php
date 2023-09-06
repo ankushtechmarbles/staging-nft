dcb2b33cf3214f1d0214524abe1ab8d67f8c9b7e
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2023-09-06 08:56:52 -0700

    Update header partial for wallet connection and user icon

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -43,17 +43,43 @@
             </div>
         </div>
 
-        <div class="d-flex align-items-center order-lg-2 ms-auto">
-            @if (auth()->user())
-                <a href="/user" class="nav-link px-2 pe-3">
+        <div class="d-flex align-items-center order-lg-2 ms-auto gap-2">
+{{--            @if (auth()->user())--}}
+                <a href="{{route('dashboard')}}" class="btn btn-warning rounded-pill d-none d-lg-inline-flex" style="padding: 0.5rem">
                     <svg class="icon-width">
                         <use xlink:href="{{ asset('assets/img/idea-icon.svg#user') }}"></use>
                     </svg>
                 </a>
-            @endif
-            @guest
-                <a href="{{route('dashboard')}}" class="btn btn-warning rounded-pill d-none d-lg-inline-flex ">Connect Wallet</a>
-            @endguest
+{{--            @endif--}}
+{{--            @guest--}}
+                <button
+                    href="{{route('dashboard')}}"
+                    class="btn btn-warning rounded-pill d-none d-lg-inline-flex"
+                    id="walletConnectBtn"
+                    data-bs-toggle="modal"
+                >
+                    Checking for wallet..
+                </button>
+
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 style="font-family: Inter, 'serif'; font-weight: 500" class="modal-title fs-5" id="exampleModalLabel">Connect Wallet</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="grid">
+                                    <div class="row justify-content-center align-items-center gap-3">
+                                        <button class="btn btn-primary col-10" id="connectMetamask">Connect MetaMask</button>
+                                        <button class="btn btn-primary col-10" id="connectPaper">Connect PaperWallet</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+{{--            @endguest--}}
         </div>
 
         {{--   Mobile    --}}
@@ -63,4 +89,62 @@
         </button>
     </div>
 </header>
+
+@section('javascript')
+    <script>
+        $(document).ready(async function() {
+
+            // check for paper
+            try {
+                if(!!(await window.PaperWallet.getAddress())) {
+                    createWalletSdk(window.PaperWallet, await window.PaperWallet.getAddress());
+                }
+            } catch (e) {
+                console.log(e);
+            }
+
+            try {
+                if(!!(await window.MetaMask.getAddress())) {
+                    createWalletSdk(window.MetaMask, await window.MetaMask.getAddress());
+                }
+            } catch (e) {
+                console.log(e);
+            }
+
+            updateWalletConnectBtn();
+
+          document.querySelector('#connectPaper').addEventListener('click', async () => {
+            await window.PaperWallet.connect((wallet) => {
+                createWalletSdk(window.PaperWallet, wallet);
+            });
+          });
+
+          document.querySelector('#connectMetamask').addEventListener('click', async () => {
+              await window.MetaMask.connect().then((wallet) => {
+                  createWalletSdk(window.MetaMask, wallet);
+              });
+          });
+
+            async function createWalletSdk(provider, address) {
+                window.walletSdk = await window.thirdweb.ThirdwebSDK.fromWallet(provider, "ethereum", {
+                    clientId: "44aa3ec3d8ffe49358a72c91c8e99e83", // Use client id if using on the client side, get it from dashboard settings
+                });
+                updateWalletConnectBtn(address);
+            }
+
+            function updateWalletConnectBtn(address= '') {
+                const walletBtn = document.querySelector('#walletConnectBtn');
+
+                // update text
+                if(window.walletSdk) {
+                    walletBtn.innerHTML = `Address: ${address.slice(0, 6)}...${address.slice(-4)}`;
+                } else {
+                    walletBtn.innerHTML = 'Connect Wallet';
+                    walletBtn.setAttribute('data-bs-target', '#exampleModal');
+                }
+            }
+        });
+    </script>
+    @parent
+@endsection
 
