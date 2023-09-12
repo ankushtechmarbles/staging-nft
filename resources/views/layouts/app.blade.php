f09b48735c73f4832ee3f6137eaa0c90d2c48570
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2023-09-11 22:30:08 -0700

    Add toast message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -90,6 +90,19 @@
       <i class="btn-scroll-top-icon bx bx-chevron-up"></i>
     </a>
 
+    {{-- toast messages --}}
+    <div class="toast-container position-fixed bottom-0 end-0 p-3">
+        <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-header">
+                <strong class="me-auto">IDEA-NFT</strong>
+                <small>Just now</small>
+                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div class="toast-body" id="toast-message">
+                Hello, world! This is a toast message.
+            </div>
+        </div>
+    </div>
 
     @livewireScripts
     <!-- Vendor Scripts -->
